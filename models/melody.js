e671e3027dfb5c37a407008e564a13fbb703d9a0
Henry Kellam <henry.a.kellam@gmail.com>
2020-02-06 15:59:56 -0500

    added melody model with crud functions;

--- models/melody.js
+++ models/melody.js
@@ -1,2 +1,70 @@
 const mongoose = require('./connection.js')
 
+// define mogoose schema and create model collection
+const MelodySchema = new mongoose.Schema({
+    title: String,
+    melody: Number
+})
+
+const MelodyCollection = mongoose.model('Melodies', MelodySchema)
+
+//function to query, create, update, and delete from collection
+function getOneMelody(id) {
+    MelodyCollection.findById(id, (err, res) => {
+        if (!err) {
+            return res
+        } else {
+            throw err
+        }
+    })
+}
+
+function getAllMelodies() {
+    MelodyCollection.find({}, (err, res) => {
+        if (!err) {
+            return res
+        } else {
+            throw err
+        }
+    })
+}
+
+function createMelody(body) {
+    MelodyCollection.create(body, (err, res) => {
+        if (!err) {
+            return res
+        } else {
+            throw err
+        }
+    })
+}
+
+function updateMelody(body, id) {
+    MelodyCollection.find(body, { _id: id }, (err, res) => {
+        if (!err) {
+            return res
+        } else {
+            throw err
+        }
+    })
+}
+
+
+function deleteMelody(id) {
+    MelodyCollection.deleteOne({ _id: id }, (err, res) => {
+        if (!err) {
+            return res
+        } else {
+            throw err
+        }
+    })
+}
+
+
+export {
+    getOneMelody,
+    getAllMelodies,
+    createMelody,
+    updateMelody,
+    deleteMelody
+}
