3ecc436239d12b8116e8383cfa0be11d2c23d1a2
Henry Kellam <henry.a.kellam@gmail.com>
2020-02-06 15:22:48 -0500

    added server file for app

---
+++ server.js
@@ -0,0 +1,24 @@
+// import dependencies
+const express = require('express')
+const app = express()
+
+//import routers from controllers
+const { appRouter } = require('./controllers/main.js')
+
+// register middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/client/build`))
+
+//add router for application use
+app.use('/api', appRouter)
+
+//catch all to serve up built react app
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
+//set port for server and start listener
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`)
+})
