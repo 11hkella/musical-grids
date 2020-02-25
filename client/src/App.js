289305fe651c396d1aca5986036b32c6222f6db4
Henry Kellam <henry.a.kellam@gmail.com>
2020-02-25 17:08:31 -0500

    initialized transport

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TransportComponent from './components/TransportComponent'
+
 
 function App() {
   return (
     <div className="App">
-
+      <TransportComponent />
     </div>
   );
 }
---
+++ client/src/components/TransportComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class TransportComponent extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Drum Machine</h1>
+            </div>
+        )
+    }
+
+}
