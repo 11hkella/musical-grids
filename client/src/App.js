e2965827e3b708f2e5ec85e201b4a850db00fe9f
Henry Kellam <henry.a.kellam@gmail.com>
2020-02-25 18:36:20 -0500

    added Step controller components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import TransportComponent from './components/TransportComponent'
-import Instrument from './components/Instrument'
 
 
 function App() {
   return (
     <div className="App">
       <TransportComponent />
-      <Instrument />
     </div>
   );
 }
--- client/src/components/TransportComponent.jsx
+++ client/src/components/TransportComponent.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import Instrument from './Instrument'
+import Steps from './Steps'
 
 export default class TransportComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            steps: [false, false, false, false, false, false, false, false,
+                false, false, false, false, false, false, false, false]
+        }
+    }
+
+    handleStepChange = (id) => {
+        const s = this.state.steps
+        s[id] = !s[id]
+        this.setState({
+            steps: s,
+        })
+    }
 
     render() {
         return (
             <div>
                 <h1>Drum Machine</h1>
+                <Instrument />
+                <Steps handleStepChange={this.handleStepChange} steps={this.state.steps} />
             </div>
         )
     }
